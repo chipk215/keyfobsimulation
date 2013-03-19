cef8547d1a6c637cbd9b0640647128c3f15916b1
chipk215 <chipk215@msn.com>
2013-03-19 16:54:14 -0600

    Updating About information

--- KeyFobSimulation/KFBAboutViewController.h
+++ KeyFobSimulation/KFBAboutViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface KFBAboutViewController : UIViewController
+@interface KFBAboutViewController : UIViewController<UIWebViewDelegate>
 
 @end
--- KeyFobSimulation/KFBAboutViewController.m
+++ KeyFobSimulation/KFBAboutViewController.m
@@ -12,6 +12,9 @@
 @property (strong, nonatomic) IBOutlet UIWebView *webView;
 - (IBAction)backButtonHandler:(UIBarButtonItem *)sender;
 
+// Activity indicator for page load
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+
 @property (weak, nonatomic) IBOutlet UILabel *errorMessageLabel;
 
 @end
@@ -36,25 +39,50 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
+    self.errorMessageLabel.text = @"";
+    self.webView.delegate = self;
     
     NSURL *url = [NSURL URLWithString:@"http://chipk215.github.com/keyfobsimulation/"];
     
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
     
+    [self.activityIndicator startAnimating];
     
-    [self.webView loadRequest:request];
-	// Do any additional setup after loading the view.
-      
-    
-    
+    dispatch_queue_t webQueue = dispatch_queue_create("download", NULL);
+    dispatch_async(webQueue, ^{
+        
+        [self.webView loadRequest:request];
+        
+    });
+
+   
+	    
    
 }
 
-- (void)didReceiveMemoryWarning
+-(void)viewWillDisappear:(BOOL)animated
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [super viewWillDisappear:animated];
+    self.webView.delegate = nil;
+    self.errorMessageLabel.text = @"";
+}
+
+#pragma mark- UIWebViewDelegate Protocol
+
+// Page loaded stop the activity indicator
+-(void)webViewDidFinishLoad:(UIWebView *)webView
+{
+    DLog(@"Web page loaded");
+    [self.activityIndicator stopAnimating];
+}
+
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
+{
+    [self.activityIndicator stopAnimating];
+    NSString *errorMessage = [NSString stringWithFormat:@"Web page failed to load.  Error:  %@",error];
+    
+    self.errorMessageLabel.text = errorMessage;
+    
 }
 
 
